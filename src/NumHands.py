9f486b25656f2b3664c4ca2d69f3ca3404d5b6c4
Rowland Creed <rowland@moralet.com>
2022-07-04 23:56:20 +0200

    [NumHands] Use integer division

--- src/NumHands.py
+++ src/NumHands.py
@@ -4,4 +4,4 @@ def NumHands(n, r): # a.k.a. "n choose r"
 	if n < r or r < 1:
 		return 0
 	# gets number of possible combinations
-	return factorial(n) / factorial(r) / factorial(n-r)
+	return factorial(n) // factorial(r) // factorial(n-r)
